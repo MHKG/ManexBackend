a03b889503041cf6b320864009d87f24423a0e96
MHKG <mehulkedia30@gmail.com>
2024-09-25 12:26:26 +0530

    Some APIS

--- src/main/java/com/manex/backend/repositories/TbSupplierPiRepository.java
+++ src/main/java/com/manex/backend/repositories/TbSupplierPiRepository.java
@@ -4,6 +4,12 @@ import com.manex.backend.entities.TbSupplierPi;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TbSupplierPiRepository
-        extends JpaRepository<TbSupplierPi, Integer>, JpaSpecificationExecutor<TbSupplierPi> {}
+        extends JpaRepository<TbSupplierPi, Integer>, JpaSpecificationExecutor<TbSupplierPi> {
+
+    @Query(
+            "SELECT tsp FROM TbSupplierPi tsp WHERE tsp.SUPP_QUOT_ID = :SUPP_QUOT_ID ORDER BY tsp.TIMESTAMP desc LIMIT 1")
+    TbSupplierPi findLatestBySuppQuotId(Integer SUPP_QUOT_ID);
+}
--- src/main/java/com/manex/backend/repositories/TbSupplierQuotationRepository.java
+++ src/main/java/com/manex/backend/repositories/TbSupplierQuotationRepository.java
@@ -4,7 +4,13 @@ import com.manex.backend.entities.TbSupplierQuotation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TbSupplierQuotationRepository
         extends JpaRepository<TbSupplierQuotation, Integer>,
-                JpaSpecificationExecutor<TbSupplierQuotation> {}
+                JpaSpecificationExecutor<TbSupplierQuotation> {
+
+    @Query(
+            "SELECT tsq FROM TbSupplierQuotation tsq WHERE tsq.SUPP_PO_ID = :SUPP_PO_ID ORDER BY tsq.TIMESTAMP desc LIMIT 1")
+    TbSupplierQuotation findLatestBySuppPoId(Integer SUPP_PO_ID);
+}
